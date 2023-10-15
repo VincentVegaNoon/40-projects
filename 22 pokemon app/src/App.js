53e54a025a20fa5212d8367996c0103375956087
VincentVegaNoon <jan.andrzej.owczarczyk@gmail.com>
2023-10-15 19:18:37 +0200

    PokemonList

--- 22 pokemon app/src/App.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react";
-import PokemonList from "./PokemonList";
-
-function App() {
-  const [pokemon, setPokemon] = useState();
-  return <PokemonList pokemon={pokemon} />;
-}
-
-export default App;
